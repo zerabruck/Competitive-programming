e9f1ed6e0da72cf7087534a0384343c6ed87bfa7
zerabruck <zerabruckm@gmail.com>
2022-04-26 09:13:57 +0300

    added new materials

--- DQ/find_original_array_from_doubled_array.py
+++ DQ/find_original_array_from_doubled_array.py
@@ -1,47 +1,22 @@
 def original(changed):
-    values=[]
-    
+    values=[] 
     if(len(changed)%2!=0 or len(changed)==0):
         return []
-    lentght=len(changed)
-    changed.sort()
-    index=0
-    counter=0
-    for i in changed:
-        if(i!=0):
-            break        
-        if (i ==0):
-            counter+=1
-            
-
-        if(counter%2==0 and counter!=0):       
-            values.append(0)  
     
+    changed.sort()
+    index=-1
     while(True):
-        try:
-            number=changed[index]*2
-        except:
-            
-            if(lentght//2==len(values)):
-                return values
-               
-                
-            else:
-                
-                return []
-        if(changed[index]==0):
-            changed.remove(0)
-            
-            continue
-        
+        if(len(changed)==0):
+            return values
+        if(changed[index]%2!=0):
+            return []
+        number=changed[index]//2
+        changed.pop(-1)
         if(number in changed):
-            
-            values.append(changed[index])
-
+            print(changed)
+            values.append(number)
             changed.remove(number)
         else:
             return []
 
-        index+=1
-
-print(original([0,0]))
+print(original([0,0,4,6,8]))
