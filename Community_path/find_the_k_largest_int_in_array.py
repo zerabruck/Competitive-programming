772e8e82923c4c694469527e37b23b1b1396c3f6
zerabruck <zerabruckm@gmail.com>
2022-10-16 22:45:04 +0300

    new questions added

--- Community_path/find_the_k_largest_int_in_array.py
+++ Community_path/find_the_k_largest_int_in_array.py
@@ -1,9 +1,14 @@
-class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        new=[int(i) for i in nums]
-        new.sort(reverse=True)
+# class Solution:
+#     def kthLargestNumber(self, nums: List[str], k: int) -> str:
+#         new=[int(i) for i in nums]
+#         new.sort(reverse=True)
         
-        return str(new[k-1])
+#         return str(new[k-1])
             
                 
         
+nums=[2,3,4,5]
+
+
+print(nums.index(4,1))
+
---
+++ Community_path/pancake_sorting.py
@@ -0,0 +1,67 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+    
+        result=[]
+        def checker(arrays):
+            temp=arrays[0]
+            for i in arrays:
+                if(temp>i):
+                    return False
+                temp=i
+            return True
+
+
+        def bigger(arrays):
+            bigger=arrays[0]
+            for i in arrays:
+                if(i>bigger):
+                    bigger=i
+            return bigger
+
+
+
+        biggest=bigger(arr)
+        counter=0
+        index=0
+        while(True):
+
+
+            if(checker(arr)==True):
+                break
+
+            for i in range(len(arr)):
+                if(biggest==arr[i]):
+                    index=i       
+                    break
+            # ------------------------------------
+
+
+
+
+        # -------------------------------------------
+
+            cutting=arr[0:index+1]
+
+
+            result.append(index+1)
+
+
+            for i in range(len(cutting)):
+                arr.pop(0)
+
+            for i in cutting:
+                arr.insert(0,i)
+            # --------------------------------------------------
+
+
+            cutting=arr[0:len(arr)-counter]
+            result.append(len(arr)-counter)
+            for i in range(len(cutting)):
+                arr.pop(0)
+
+            for i in cutting:
+                arr.insert(0,i)
+
+            counter+=1
+            biggest-=1
+        return result
