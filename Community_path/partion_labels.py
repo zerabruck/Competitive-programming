150bf0c64a189f6ebcd0826ef6c6b02b3c8711f6
zerabruck <zerabruckm@gmail.com>
2022-10-18 11:42:53 +0300

    new questions added

---
+++ Community_path/partion_labels.py
@@ -0,0 +1,42 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+
+        dicto={}
+        checking_arr=[]
+        result=[]
+        def checker(val):
+            dicto[val[-1]]-=1        
+            for i in val:
+
+                if(dicto[i]!=0):
+
+
+                    return False
+            return True
+
+        for i in s:
+            if(i not in dicto):
+                dicto[i]=1
+
+            else:
+                dicto[i]+=1
+
+
+        first_point=0
+        second_point=0
+
+        for i in s:
+
+
+            checking_arr.append(i)
+            if(checker(checking_arr)):
+
+
+                result.append(len(checking_arr))
+                checking_arr.clear() 
+                
+                
+        return result
+          
+                
