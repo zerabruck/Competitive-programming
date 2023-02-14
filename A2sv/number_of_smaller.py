b49b31ecf35c38d08a3fb47e43c79e2b2be73a0f
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-02-14 05:42:29 +0300

    Create number_of_smaller.py

---
+++ A2sv/number_of_smaller.py
@@ -0,0 +1,17 @@
+input()
+first_arr = list(map(int,input().split()))
+second_arr = list(map(int,input().split()))
+result = []
+ 
+first_pointer = 0
+ 
+for num in second_arr:
+    while first_pointer != len(first_arr):
+        if  num <= first_arr[first_pointer]:
+            break
+ 
+        first_pointer += 1
+ 
+    result.append(first_pointer)
+ 
+print(*result)
