aaf356c05214baf37f68c989c68f05c0571dad7b
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-03-03 20:26:37 +0300

    Create range_sum_query_immutableII.py

---
+++ A2sv/range_sum_query_immutableII.py
@@ -0,0 +1,30 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        zeros = [0] * len(matrix[0])
+        self.matrix = [zeros]
+        self.matrix.extend(matrix)
+
+        for row in self.matrix:
+            row.insert(0 , 0)
+
+
+        for row in range(1 , len(self.matrix)):
+            for col in range(1 , len(self.matrix[row])):
+                self.matrix[row] [col] = self.matrix[row - 1 ] [col] + self.matrix[row] [col - 1] + self.matrix[row] [col] - self.matrix[row - 1 ] [col - 1]
+
+        
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        row1 += 1
+        row2 += 1
+        col1 += 1
+        col2 += 1
+
+        return self.matrix[row2][col2] + self.matrix[row1 - 1] [col1 - 1] - self.matrix[row2][col1 - 1] - self.matrix[row1 - 1] [ col2]
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
