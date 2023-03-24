d51a36ae20eb18fb9e7b22aec750617b58aca855
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-03-24 18:36:25 +0300

    Create missing_numbers.py

---
+++ A2sv/missing_numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.append(-1)
+        first = 0
+        while first < len(nums):
+            if nums[first] == -1 or nums[first] == first:
+                first += 1
+
+            else:
+                val = nums[first]
+                nums[first] , nums[val] = nums[val] , nums[first]
+
+        for indx , val in enumerate(nums):
+            if val == -1:
+                return indx
