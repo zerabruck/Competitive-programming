8c9deb2bab5bbf83e06e8e7dea77814814556d7a
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-03-30 19:42:54 +0300

    Create hamming_distance.py

---
+++ A2sv/hamming_distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        diff = x ^ y
+        count = 0
+        while diff > 0:
+            if diff & 1 == 1:
+                count += 1
+            diff >>= 1
+        return count
