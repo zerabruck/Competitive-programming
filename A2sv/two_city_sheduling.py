5eee775af0b3c1e62fb09dc63e73c5b5e9090b2a
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-10-10 14:27:04 +0300

    Create two_city_sheduling.py

---
+++ A2sv/two_city_sheduling.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key = lambda x:x[0] - x[1])
+        cost = 0
+        for i in range(len(costs)//2):
+            cost += costs[i][0]
+            cost += costs[~i][1]
+        return cost
