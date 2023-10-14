272a87d0a64201fbb09443aaa119df6b380736f5
zerabruck muluneh <91604009+zerabruck@users.noreply.github.com>
2023-10-14 10:08:30 +0300

    Create design_hash_map.py

---
+++ A2sv/design_hash_map.py
@@ -0,0 +1,27 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.dict = {}
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.dict[key] = value
+        
+
+    def get(self, key: int) -> int:
+        if key not in self.dict:
+            return -1
+        return self.dict[key]
+        
+
+    def remove(self, key: int) -> None:
+        if key in self.dict:
+            del self.dict[key]
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
